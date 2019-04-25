86cf0306d2dc2198f66981b63c4908a29e42cfed
Matthew Bressler <matth.bressler@gmail.com>
2019-04-25 12:01:01 -0400

    Changed table schema

--- p2c/schema.sql
+++ p2c/schema.sql
@@ -1,7 +1,6 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS pick;
 
---user is a pparently a reserved word so i changed it to people
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   username TEXT UNIQUE NOT NULL,
@@ -14,5 +13,5 @@ CREATE TABLE pick (
   username TEXT NOT NULL, 
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   player TEXT NOT NULL,
-  click BIT DEFAULT NULL
+  click BOOLEAN DEFAULT NULL
 );
